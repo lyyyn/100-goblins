d1b34510c03717afe994b1b0dae94ea9668e4529
Linawaty <linawatylim@gmail.com>
2020-07-07 20:43:55 +0800

    refracted

--- 100Goblins.js
+++ 100Goblins.js
@@ -1,25 +1,17 @@
-const goblins = [];
 const gobNum = 100;
-let counter = 1;
+const goblins = new Array(gobNum).fill(false); //initiate the sick goblins
 const result = [];
 
-//initiate the sick goblins
-for (let i = 0; i < gobNum; i++) {
-    goblins.push(false);
-}
-
 //curing goblins
-for (let i = 1; i <= gobNum; i++) {
-    for (let j = i; j <= gobNum; j += i) {
-        goblins.splice(j - 1, 1, !goblins[j - 1]);
+goblins.forEach((gob, idx) => {
+    for (let j = idx + 1; j <= gobNum; j += idx + 1) {
+        goblins[j - 1] = !goblins[j - 1];
     }
-}
+})
 
 //pick only the cured goblins
 goblins.forEach((element, idx) => {
-    if (element === true) {
-        result.push(idx + 1);
-    }
+    element && result.push(idx + 1);
 });
 
 console.log(result);
