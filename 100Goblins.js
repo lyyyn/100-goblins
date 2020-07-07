1efb4e4e522e31d1a10a515999fdf262850c6570
Linawaty <linawatylim@gmail.com>
2020-07-07 19:54:06 +0800

    finished iterate the curing 100 goblins

--- 100Goblins.js
+++ 100Goblins.js
@@ -1,10 +1,16 @@
 const goblins = [];
+const gobNum = 10;
 let counter = 1;
 
+
 //initiate the sick goblins
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= gobNum; i++) {
     goblins.push(false);
 }
 
-console.log(goblins);
+for (let i = 1; i <= gobNum; i++) {
 
+    for (let j = i; j <= gobNum; j += i) {
+        goblins.splice(j - 1, 1, !goblins[j - 1]);
+    }
+}
