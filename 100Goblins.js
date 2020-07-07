1ca102cf9525d6d20a1444f1a3ea3f887f2a0a65
Linawaty <linawatylim@gmail.com>
2020-07-07 20:03:21 +0800

    picked only the cured goblins

--- 100Goblins.js
+++ 100Goblins.js
@@ -1,16 +1,25 @@
 const goblins = [];
-const gobNum = 10;
+const gobNum = 100;
 let counter = 1;
-
+const result = [];
 
 //initiate the sick goblins
-for (let i = 1; i <= gobNum; i++) {
+for (let i = 0; i < gobNum; i++) {
     goblins.push(false);
 }
 
+//curing goblins
 for (let i = 1; i <= gobNum; i++) {
-
     for (let j = i; j <= gobNum; j += i) {
         goblins.splice(j - 1, 1, !goblins[j - 1]);
     }
 }
+
+//pick only the cured goblins
+goblins.forEach((element, idx) => {
+    if (element === true) {
+        result.push(idx + 1);
+    }
+});
+
+console.log(result);
